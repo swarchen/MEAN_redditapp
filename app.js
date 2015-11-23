8b16908336d190d64095fafc8e4d231fad55eb71
jhaocheng <jhaocheng.wu@gmail.com>
2015-11-24 00:37:12 +0800

    frontend finished

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-var app = angular.module('flapperNews',[]);
+var app = angular.module('flapperNews',['ui.router']);
 
 app.factory('posts',[function(){
 	var o = {
-		posts:[]
+		posts:[{
+			title:'Taiwan',
+			link :'http://www.Taiwan.com',
+			comments:[],
+			upvotes:23
+		}]
 	};
 	return o;
 }]);
@@ -17,7 +22,11 @@ app.controller('MainCtrl', ['$scope','posts',function($scope,posts){
 		$scope.posts.push({
 			title:$scope.post.title,
 			link :$scope.post.link,
-			upvotes:0
+			upvotes:0,
+			comments: [
+		    	{author: 'Joe', body: 'Cool post!', upvotes: 0},
+			    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		    ]
 		});
 		$scope.post.link = "";
 		$scope.post.title = "";
@@ -28,3 +37,51 @@ app.controller('MainCtrl', ['$scope','posts',function($scope,posts){
 	}
 
 }]);
+
+app.controller('PostsCtrl', ['$scope','$stateParams','posts', //順序很重要 = =
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function(){
+			if($scope.body === '') {return ;}
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = ""
+		};
+
+		$scope.incrementUpvote = function(comment){
+			comment.upvotes += 1;
+		}
+	}
+
+	
+])
+
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+		
+		$stateProvider
+		.state('home',{
+			url: '/home',
+			templateUrl: 'home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts',{
+			url:'/posts/:id',
+			templateUrl:'posts.html',
+			controller: 'PostsCtrl'
+		});
+
+		$urlRouterProvider.otherwise('home');
+	}
+]);
+
+
+
+
+
